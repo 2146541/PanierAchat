6fc3d313d0fb42e785bb6f49b8991b05d50a3342
2146541 <94549772+2146541@users.noreply.github.com>
2021-11-21 13:06:45 -0500

    Add project files.

---
+++ Panier.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PanierAchat
+{
+    public class Panier
+    {
+        // nom de la Personne
+        private string noItem;
+        private string nomItem;
+        private string tailleItem;
+        private decimal prixItem;
+
+        //Propriété Auto-implémentée
+        public string CodeItem { get; set; }
+        public string NameItem { get; set; }
+        public string SizeItem 
+        { 
+            get 
+            { 
+                return tailleItem; 
+            } 
+            set 
+            {   
+                if (value == "0")
+                    tailleItem = "Taille inconnue";
+                else
+                    tailleItem = value;
+            } 
+        }
+        public decimal PriceItem { 
+            get 
+            { 
+                return Convert.ToDecimal(prixItem); 
+            } 
+            set 
+            {   
+                if (value < 0)                  
+                    prixItem = 0.0m; 
+                else
+                    prixItem = Convert.ToDecimal(value);
+            }
+        }
+
+        public Panier()
+        {
+            //CodeItem = null;
+            //NameItem = null;
+            //SizeItem = null;
+            //PriceItem = 0.0m;
+            noItem = null;
+            nomItem = null;
+            tailleItem = null;
+            prixItem = 0.0m;
+        }
+
+        // Constructeur initialise Personne
+        public Panier(string noItem, string nomItem, string tailleItem, decimal prixItem)
+        {
+            //CodeItem = noItem;
+            //NameItem = nomItem;
+            //SizeItem = tailleItem;
+            //PriceItem = Convert.ToDecimal(prixItem);
+            this.noItem = noItem;
+            this.nomItem = nomItem;
+            this.tailleItem = tailleItem;
+            this.prixItem = Convert.ToDecimal(prixItem);
+        }
+
+
+        public override string ToString()
+        {
+            return this.noItem + " " + this.nomItem + " " + this.tailleItem + " " + this.prixItem;
+        }
+
+        // Affiche un message à l'utilisateur
+        public void AfficheMessage()
+        {
+            Console.WriteLine("Item #: {0}\nDescription: {1}\nTaille: {2}\nPrix unitaire: {3}\n", this.noItem, this.nomItem, this.tailleItem, this.prixItem);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PanierAchat
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Créer un objet Personne
+            Panier objPanier1 = new Panier();
+
+            Console.WriteLine("Entrer le code article: ");
+            objPanier1.CodeItem = Console.ReadLine();
+            Console.WriteLine("Entrer le nom de l'article: ");
+            objPanier1.NameItem = Console.ReadLine();
+            Console.WriteLine("Entrer la taille de l'article: ");
+            objPanier1.SizeItem = Console.ReadLine();
+            Console.WriteLine("Entrer le prix de l'article: ");
+            objPanier1.PriceItem = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("==============================");
+            Console.WriteLine("Détails Facturette\n");
+            Console.WriteLine("============================");
+
+            // Appel de la méthode de l'objet AfficheMessage()
+            Console.WriteLine("Item #: {0}\nDescription: {1}\nTaille: {2}\nPrix unitaire: {3} $\n", objPanier1.CodeItem, objPanier1.NameItem, objPanier1.SizeItem, objPanier1.PriceItem);
+            Console.WriteLine("==============================");
+
+        } // Fin du Main
+    } // Fin de la Classe Program
+}
+
